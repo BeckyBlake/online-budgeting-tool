9b7ad3012f19c1afe798d930cbd566e902d45883
Becky Blake <ryblake2@illinois.edu>
2023-04-12 12:23:15 -0500

    clean up comments

--- budget_tracker_website/src/App.js
+++ budget_tracker_website/src/App.js
@@ -1,35 +1,13 @@
-// import './App.css';
-// import React from 'react';
-// import './App.css';
-// import Dashboard from './Components/Dashboard/Dashboard';
-// import Login from './Components/Login/Login';
-// import Preferences from './Components/Preferences/Preferences';
-// import useToken from './Components/App/useToken';
-
-
-//  // eslint-disable-next-line
-// // import { useState } from 'react';
-// import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
- // eslint-disable-next-line
-// import { useHistory } from 'react-router-dom';
-
-// import {Home} from './Components/Home';
-// import {About} from './Components/About';
-// import {Project} from './Components/Project';
-
-
 import React from 'react';
-import { Route, Routes, useHref } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import './App.css';
 import Dashboard from './Pages/Dashboard';
 import Contact from './Pages/Financial Literacy';
 import About from './Pages/Budgets';
 import Login from './Pages/Login/Login';
 import useToken from './useToken';
-import NavBar from './Components/NavBar/index';
 import NavigationBar from './Components/NavigationBar/NavigationBar';
 import Liquid_Lizard from './assets/liquid_lizard.png';
-import {Link} from "react-router-dom"
 
 function App() {
   const { token, setToken } = useToken();
